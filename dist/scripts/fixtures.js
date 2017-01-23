d39da6c513a292889be9a307ca8a9236004a210d
Travis Rodgers <rodgtr1@gmail.com>
2017-01-23 18:13:20 -0500

    fixed bug due to mistype

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumKennyG = {
+     title: 'Saxes of Fire',
+     artist: 'Kenny G',
+     label: 'Circular Breathing',
+     year: '1995',
+     albumArtUrl: 'assets/images/album_covers/11.png',
+     songs: [
+         { title: 'Where is my reed', duration: '3:51' },
+         { title: 'My spit valve is sealed tight', duration: '2:54' },
+         { title: 'Leaky pads', duration: '0:21'},
+         { title: 'Conga Mamba', duration: '4:20' },
+         { title: 'Octaves, your one has nothing on my four', duration: '4:00'}
+     ]
+ };
